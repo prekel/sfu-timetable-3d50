e5586127d512187dd0d9457d228d8795319bd044
Vladislav Prekel <misterptits@yandex.ru>
2021-07-16 19:20:50 +0700

    without ink

--- sfu-timetable-ink/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from "react";
-import { Text } from "ink";
-
-export const App: FC<{ name?: string }> = ({ name = "Stranger" }) => (
-  <Text>
-    Hello, <Text color="green">{name}</Text>
-  </Text>
-);
-
